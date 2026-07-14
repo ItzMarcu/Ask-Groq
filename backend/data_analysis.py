0e0fad96e478db0f26445dab544d7c563c245f38
Andrea Marcucci <andreamrccc@gmail.com>
2026-07-14 23:58:57 +0200

    First Layout, no test yet

---
+++ backend/data_analysis.py
@@ -0,0 +1,30 @@
+from groq import Groq
+from dotenv import load_dotenv
+from os import getenv
+
+def client(): 
+    load_dotenv()
+    return Groq(api_key=getenv("API_KEY"))
+
+def analyze_data(data: str = None) -> str:
+    if not data: 
+        return {"error": "no data given"}
+    
+    llm = client()
+    message = [
+        {
+            "role": "user",
+            "content": f"elabora questo testo e valuta se dare una risposta contente le informazioni" /
+            "riportate senza aggiungere alcuna intestazione rendendole anche facilmente leggibili" /
+            "oppure nel caso sia una domanda in merito a tale argomento fornisci in breve un parametro" / 
+            "di una sola parola per ottenere piú informazioni a riguardo da passare alla query API" / 
+            "in tal caso scrivi la parola in snake case e non usare spazi"
+        }
+    ]
+
+    response = llm.chat.completions.create(messages=message, model="llama-3.1-8b-instant").choices[0].message.content
+    
+    if len(response.split()) == 1: 
+        return (response, True)
+    
+    return response
---
+++ backend/web_scrape.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from requests import get, RequestException
+from fastapi import HTTPException
+
+def scrape(url: str = None): 
+    if not url: 
+        return {"errore": "no url given"}
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+        "Accept-Language": "it-IT,it;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1"
+    }
+
+    try: 
+        response = get(url, headers=headers, timeout=15)
+        if response.status_code == 200: 
+            soup = BeautifulSoup(response.text, "html.parser")
+            data = soup.find(id="mw-content-text").text
+
+            if not data: 
+                raise HTTPException(status_code=404, id="data not found")
+            
+            return data
+    
+    except RequestException as e:
+        raise HTTPException(status_code=500, detail=str(e))
